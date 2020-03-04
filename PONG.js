f7247b4e4017b25b12693c535f19773fb769ffc0
1004005 <59642351+1004005@users.noreply.github.com>
2020-03-04 09:36:03 +0100

    Update PONG.js

--- PONG.js
+++ PONG.js
@@ -1,9 +1,29 @@
+let backgroundColor=0;
+let canvasWidth=600;
+let canvasHeight=400;
+let paddleWidth=5;
+let paddleHeight=60;
+let paddleY = canvasHeight/2 - paddleHeight/2;
+let ballY = 195;
+let ballX = 295;
+let velocity= 2;
+let leftPaddleX= 10;
+let rightPaddleX= 585; 
+
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(canvasWidth, canvasHeight);
 }
 
 function draw() {
-  background(0);
-  rect(10,170,5,60);
-  rect(585,170,5,60);
+  
+  background(backgroundColor);
+  rect(leftPaddleX,paddleY,paddleWidth,paddleHeight);
+  rect(rightPaddleX,paddleY,paddleWidth,paddleHeight);
+  ellipse(ballX,ballY,10);
+  
+  ballX= ballX+velocity;
+  
+  if(ballX > rightPaddleX || ballX < leftPaddleX) {
+    velocity = -velocity
+  }
 }
